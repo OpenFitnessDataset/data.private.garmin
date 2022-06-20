c50e64d712aed36a0358ee0eaf724b4dd8458218
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-20 13:38:50 +0200

    working on tools

---
+++ scripts/steamlineDirectories.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+path = None
+
+
+def main():
+    global path
+    print( 'streamline directories' )
+    print( 'input: ' )
+    inpDir = input()
+    path = inpDir
+
+    searchForDirectories()
+    pass
+
+
+def searchForDirectories():
+    global path
+    arr = os.listdir( path )
+
+    for x in arr:
+        newPath = x.split('-')[2]
+
+        outputOld = path + '\\' + x
+        print(outputOld)
+
+        
+
+
+if (__name__ == '__main__'):
+    main()
